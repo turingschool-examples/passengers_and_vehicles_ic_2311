45e68850ea913a74fea003083ea78aea97b48ac7
Dana Howell <howelld115@gmail.com>
2023-12-13 10:08:07 -0700

    I2 refactors vehicle_spec and completes #speeding?, #speed, #add_passenger, #num_adults for Vehicle

---
+++ lib/vehicle.rb
@@ -0,0 +1,35 @@
+class Vehicle
+  attr_reader :year,
+              :make,
+              :model,
+              :passengers
+
+  def initialize(year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @is_speeding = false
+    @passengers = []
+  end
+
+  def speeding?
+    @is_speeding
+  end
+
+  def speed
+    @is_speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
+
+  def num_adults
+    @passengers.count do |passenger|
+      passenger if passenger.age >= 18
+    end
+  end
+
+
+  
+end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,41 +3,35 @@ require './lib/passenger'
 
 RSpec.describe Vehicle do
   before do
-    vehicle = Vehicle.new("2001", "Honda", "Civic")  
-    charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-    #<Passenger:0x00007fe0da1ec450...>
-    jude = Passenger.new({"name" => "Jude", "age" => 20}) 
-    #<Passenger:0x00007fe0da2730e0...>
-    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-    #<Passenger:0x00007fe0da2cf1b0...>
+    @vehicle = Vehicle.new("2001", "Honda", "Civic")  
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   it 'has attributes' do
-    expect(vehicle.year).to eq("2001")
-    expect(vehicle.make).to eq("Honda")
-    expect(vehicle.model).to eq("Civic")
+    expect(@vehicle.year).to eq("2001")
+    expect(@vehicle.make).to eq("Honda")
+    expect(@vehicle.model).to eq("Civic")
   end
 
-  it 'can assess if vehicle is #speeding' do
-    expect(vehicle.speeding?).to eq(false)
+  it 'can assess if @vehicle is #speeding' do
+    expect(@vehicle.speeding?).to eq(false)
 
-    vehicle.speed
+    @vehicle.speed
     
-    expect(vehicle.speeding?).to eq(true)
+    expect(@vehicle.speeding?).to eq(true)
   end
 
   it 'can #add_passenger and count #num_adults' do
-    expect(vehicle.passengers).to eq([])
+    expect(@vehicle.passengers).to eq([])
 
-    vehicle.add_passenger(charlie)    
-    vehicle.add_passenger(jude)    
-    vehicle.add_passenger(taylor)    
+    @vehicle.add_passenger(@charlie)    
+    @vehicle.add_passenger(@jude)    
+    @vehicle.add_passenger(@taylor)    
     
-    expect(vehicle.passengers).to eq()
-    #[#<Passenger:0x00007fe0da1ec450...>, #<Passenger:0x00007fe0da2730e0...>, #<Passenger:0x00007fe0da2cf1b0...>]
-    expect(vehicle.num_adults).to eq()
-    2
-    #<Vehicle:0x00007fe0da9c63d8...>
+    expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
+    expect(@vehicle.num_adults).to eq(2)
   end
 
 end
