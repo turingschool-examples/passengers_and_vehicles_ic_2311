e6131d64a4647a324292b645a8f8eaef2f090be4
Joey Reyes <joeyreyes105@gmail.com>
2023-12-13 09:15:36 -0800

    Add number of adults method

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -17,4 +17,10 @@ class Vehicle
     def add_passenger(passenger)
         passengers << passenger
     end 
+
+    def num_adults 
+        passengers.count do |passenger|
+            passenger.adult?
+        end 
+    end 
 end 
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -65,4 +65,21 @@ RSpec.describe Vehicle do
             expect(vehicle.passengers).to eq([charlie, jude, taylor])
         end 
     end 
+
+    describe '#num_adults' do 
+        it 'counts the number of adults' do 
+            vehicle = Vehicle.new("2001", "Honda", "Civic")
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            vehicle.add_passenger(charlie)
+
+            expect(vehicle.num_adults).to eq(1)
+
+            jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+            vehicle.add_passenger(jude)
+            vehicle.add_passenger(taylor)
+
+            expect(vehicle.num_adults).to eq(2)
+        end 
+    end
 end 
