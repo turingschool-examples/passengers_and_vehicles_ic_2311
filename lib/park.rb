4d1c203158f4f228732f9e2f93933deec8d8353b
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 09:28:53 -0800

    feat: initialize commit

---
+++ lib/park.rb
@@ -0,0 +1,16 @@
+require './lib/passenger'
+require './lib/vehicle'
+
+class Park
+  attr_reader :name,
+              :addmission_price,
+              :vehicles
+
+  def initialize(name, addmission_price)
+    @name = name
+    @addmission_price = addmission_price
+    @vehicles = []
+  end
+
+
+end
