c1ca493b218e89c3f50b59965c962f93f2c05dbc
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:12:53 -0500

    Feat: create add vehicles method and vehicles entered park instance variable

--- lib/park.rb
+++ lib/park.rb
@@ -1,9 +1,15 @@
 class Park
    attr_reader :name,
-               :admission_price
+               :admission_price,
+               :vehicles_entered_park
    
    def initialize(name, admission_price)
       @name = name
       @admission_price = admission_price
+      @vehicles_entered_park = []
+   end
+
+   def add_vehicle(vehicle)
+      @vehicles_entered_park.push(vehicle)
    end
 end
