393a7fb49919baf057cc5f91bbfd152fc1c35a18
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 10:41:10 -0700

    Pass add_vehicle test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,4 +10,16 @@ describe Park do
       expect(the_medows.park_data).to eq("Welcome to The Medows park, the admission fee is 15 dollars")
     end
   end
+
+  describe '#add_vehicle' do
+    it 'adds a vehicle to the park' do
+      the_medows = Park.new("The Medows", 15)
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      the_medows.add_vehicle(vehicle)
+      expect(the_medows.vehicles).to include(vehicle)
+    end
+  end
+
+  
 end
