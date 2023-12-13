c58f752bcec50eb3572dcdc44914e6fc9b8f8c39
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:21:40 -0500

    Feat: add @passenger_entered_park instance variable and attribute reader

--- lib/park.rb
+++ lib/park.rb
@@ -1,15 +1,20 @@
 class Park
    attr_reader :name,
                :admission_price,
-               :vehicles_entered_park
+               :vehicles_entered_park,
+               :passengers_entered_park
    
    def initialize(name, admission_price)
       @name = name
       @admission_price = admission_price
       @vehicles_entered_park = []
+      @passengers_entered_park = []
    end
 
    def add_vehicle(vehicle)
       @vehicles_entered_park.push(vehicle)
+      @vehicles_entered_park.each { |vehicle| vehicle.passengers.each{ |passenger| @passengers_entered_park.push(passenger) } }
+      @vehicles_entered_park
    end
+
 end
