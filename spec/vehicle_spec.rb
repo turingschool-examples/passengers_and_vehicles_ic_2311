90d92684149c9b14e27c6839effeefbcdbee0874
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 10:12:11 -0700

    Pass num_adults test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -48,4 +48,19 @@ describe Vehicle do
       expect(vehicle.passengers).to eq([charlie, jude, taylor])
     end
   end
+
+  describe '#num_adults' do
+    it 'counts how many adults are in a vehicle' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.num_adults).to eq(2)
+    end
+  end
 end
