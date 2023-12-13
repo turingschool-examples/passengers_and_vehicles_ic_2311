4cacbd849bdd992448f255b53b7c763d631a91c2
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 11:15:33 -0500

    Test: create test for name in initialize method

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe do
          expect(passenger).to be_a Passenger
       end
 
+      it 'has a name' do
+         passenger = Passenger.new({"name" => "Charlie", "age" => 18})
+         expect(passenger.name).to eq "Charlie"
+      end
+
+      
    end
 end
