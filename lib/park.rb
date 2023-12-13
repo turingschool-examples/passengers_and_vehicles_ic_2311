dd88755dbe6b6f2a4403ce49660a79ad1d315660
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 10:01:33 -0800

    feat:calculate_revenue

--- lib/park.rb
+++ lib/park.rb
@@ -3,16 +3,26 @@ require './lib/vehicle'
 
 class Park
   attr_reader :name,
-              :addmission_price,
+              :admission_price,
               :vehicles
 
-  def initialize(name, addmission_price)
+  def initialize(name, admission_price)
     @name = name
-    @addmission_price = addmission_price
+    @admission_price = admission_price
     @vehicles = []
   end
 
   def add_vehicle(vehicle)
     @vehicles << vehicle
+  end
+
+  def list_of_passengers
+    @vehicles.flat_map(&:passengers)
+  end
+
+  def calculate_revenue
+    adult_passengers = list_of_passengers.find_all(&:adult?)
+    revenue = adult_passengers.size * @admission_price
+    revenue
   end
 end
