b6e54202a5262972059476ce02d98e54ec80fdbf
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 09:08:29 -0800

    feat: return the speeding status

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,12 +1,22 @@
 class Vehicle
   attr_reader :year,
               :make,
-              :model
+              :model,
+              :speeding
 
-  def initialize(year, make, model)
+  def initialize(year, make, model, speeding = false)
     @year = year
     @make = make
     @model = model
+    @speeding = speeding
+  end
+
+  def speeding?
+    @speeding
+  end
+
+  def speed
+    @speeding = true
   end
 
 
