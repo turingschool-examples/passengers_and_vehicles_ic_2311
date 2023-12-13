ea8083d470025a9029007552ed5edf4e46c4d27d
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 10:47:36 -0700

    Pass revenue test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -39,5 +39,20 @@ describe Park do
   end
 
 
+  describe '#revenue' do
+    it 'calculates revenue based on adult passengers' do
+      the_medows = Park.new("The Medows", 15)
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
 
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+      the_medows.add_vehicle(vehicle)
+
+      expect(the_medows.revenue).to eq(30)
+    end
+  end
 end
