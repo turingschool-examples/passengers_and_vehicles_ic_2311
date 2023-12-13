7c3a2baca4d92a79311cb1b4429acdde4b604432
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 11:37:51 -0500

    Test: Add tests for instance variables year, make and model

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe Vehicle do
       it 'can initialize' do
          expect(@vehicle).to be_a Vehicle
       end
+
+      it 'has a year' do
+         expect(@vehicle.year).to eq "2001"
+      end
+
+      it 'has a make' do
+         expect(@vehicle.make).to eq "Honda"
+      end
+
+      it 'has a model' do
+         expect(@vehicle.model).to eq "Civic"
+      end
    end
 end
