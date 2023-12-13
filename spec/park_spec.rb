a7a2556b66ca5b7c11f31648f36a92a7fda80df5
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 09:35:17 -0800

    test: add_vehicle

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Park do
-  let(:yosemite) {Park.new("Yosemite", "$50")}
+  let(:yosemite) {Park.new("Yosemite", 50)}
+  let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")}
 
   describe "#initialize" do
     it "can initialize" do
       expect(yosemite).to be_a (Park)
       expect(yosemite.name).to eq ("Yosemite")
-      expect(yosemite.addmission_price).to eq ("$50")
+      expect(yosemite.addmission_price).to eq (50)
       expect(yosemite.vehicles).to eq ([])
+    end
+  end
+
+  describe "#add_vehicle" do
+    it "add a vehicle to the park" do
+      yosemite.add_vehicle(vehicle)
+
+      expect(yosemite.vehicles).to eq ([vehicle])
     end
   end
 end
