9bc47127201c08c53d7db87c787cc09ca5920d8b
Aurora Sanderson <AuroraTuring@gmail.com>
2023-12-13 10:23:35 -0700

    Add expect statements from their interaction pattern.

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -6,69 +6,33 @@ require "./lib/vehicle"
 RSpec.describe Vehicle do
   before(:each) do
     @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   describe "#passenger" do 
     it "has attributes" do 
       expect(@vehicle).to be_an_instance_of(Vehicle)
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
-      expect().to eq()
+      expect(@vehicle.year).to eq("2001")
+      expect(@vehicle.make).to eq("Honda")
+      expect(@vehicle.model).to eq("Civic")
+      expect(@vehicle.speeding?).to eq(false)
 
+      @vehicle.speed
+
+      expect(@vehicle.speeding?).to eq(true)
+      expect(@vehicle.passengers).to eq([])
+
+      @vehicle.add_passenger(charlie)
+      @vehicle.add_passenger(jude)
+      @vehicle.add_passenger(taylor)
+
+      expect(@vehicle.passengers).to eq(:passenger)
+      expect(@vehicle.num_adults).to eq(2)
+    end
+  end
 
 
 end
 
-
-
-
-```ruby
-
-pry(main)> vehicle.year
-# => "2001"
-
-pry(main)> vehicle.make
-# => "Honda"
-
-pry(main)> vehicle.model
-# => "Civic"
-
-pry(main)> vehicle.speeding?
-# => false
-
-pry(main)> vehicle.speed
-
-pry(main)> vehicle.speeding?
-# => true
-
-pry(main)> vehicle.passengers
-# => []
-
-pry(main)> charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
-# => #<Passenger:0x00007fe0da1ec450...>
-
-pry(main)> jude = Passenger.new({"name" => "Jude", "age" => 20})    
-# => #<Passenger:0x00007fe0da2730e0...>
-
-pry(main)> taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
-# => #<Passenger:0x00007fe0da2cf1b0...>
-
-pry(main)> vehicle.add_passenger(charlie)    
-
-pry(main)> vehicle.add_passenger(jude)    
-
-pry(main)> vehicle.add_passenger(taylor)    
-
-pry(main)> vehicle.passengers
-# => [#<Passenger:0x00007fe0da1ec450...>, #<Passenger:0x00007fe0da2730e0...>, #<Passenger:0x00007fe0da2cf1b0...>]
-
-pry(main)> vehicle.num_adults
-# => 2
-```
