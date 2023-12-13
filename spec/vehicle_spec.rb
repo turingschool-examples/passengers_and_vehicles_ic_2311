698970ccbcb8152fe60ad514b3b37bc0d37c6017
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 11:53:38 -0500

    Test: create test for num_adults method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -55,4 +55,17 @@ RSpec.describe Vehicle do
          expect(@vehicle.passengers).to eq ([charlie, jude, taylor])
       end
    end
+
+   describe '#num_adults' do
+      it 'return the num of adults in the vehicle' do
+         charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+         jude = Passenger.new({"name" => "Jude", "age" => 20})
+         taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+         @vehicle.add_passenger(charlie)
+         @vehicle.add_passenger(jude)
+         @vehicle.add_passenger(taylor)
+         expect(@vehicle.num_adults).to eq 2
+      end
+   end
 end
