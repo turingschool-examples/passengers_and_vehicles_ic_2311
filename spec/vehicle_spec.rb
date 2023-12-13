b74069844e7bacb3cde346eed1fe823632ba77b4
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 10:03:39 -0700

    Pass addd_passenger test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -31,4 +31,21 @@ describe Vehicle do
       expect(vehicle.speeding?).to eq(true)
     end
   end
+
+  describe '#add_passenger' do
+    it 'can add a passenger' do
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.passengers).to eq([])
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    end
+  end
 end
