e5bb4e8971fbb719555e86018cdf47c74602744d
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 09:33:14 -0700

    Add to exists test

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -8,14 +8,23 @@ describe Passenger do
       passenger = Passenger.new({"name" => "Charlie", "age" => 18})
 
       expect(passenger).to be_instance_of(Passenger)
+      expect(passenger.name).to eq("Charlie")
+      expect(passenger.age).to eq(18)
+      expect(passenger.driver?).to eq(false)
     end
   end
 
   describe '#adult?' do
     it 'returns true when age is 18 or older' do
-      Charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
 
-      expect(Charlie.adult?).to eq(true)
+      expect(charlie.adult?).to eq(true)
+    end
+
+    it 'returns false when age is less than 18' do
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      expect(taylor.adult?).to eq(false)
     end
   end
 end
