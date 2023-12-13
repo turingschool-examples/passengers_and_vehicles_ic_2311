bc175332f1cdbe779b9e012151a39ec572ff9b8e
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:32:22 -0500

    Feat: include revenue to initialize and attr_reader

--- lib/park.rb
+++ lib/park.rb
@@ -2,13 +2,15 @@ class Park
    attr_reader :name,
                :admission_price,
                :vehicles_entered_park,
-               :passengers_entered_park
+               :passengers_entered_park,
+               :revenue
    
    def initialize(name, admission_price)
       @name = name
       @admission_price = admission_price
       @vehicles_entered_park = []
       @passengers_entered_park = []
+      @revenue = 0
    end
 
    def add_vehicle(vehicle)
