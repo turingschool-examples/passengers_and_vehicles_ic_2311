02a538e3a966112573cccf341cc5423dddb52492
Selena Hawamdeh <selenahawamdeh@gmail.com>
2023-12-13 09:54:17 -0700

    Pass speeding? test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -12,4 +12,12 @@ describe Vehicle do
       expect(vehicle.model).to eq("Civic")
     end
   end
+
+  describe '#speeding' do
+    it 'returns false by defult' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.speeding?).to eq(false)
+    end
+  end
 end
