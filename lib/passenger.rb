ae57a77244f306a2a6dffd05374f1c1575211686
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 11:26:37 -0500

    Feat: include driver to initialize method and attribute reader; Add #driver? method to call attribute

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,13 +1,19 @@
 class Passenger
    attr_reader :name, 
-               :age
+               :age,
+               :driver
    
    def initialize(passenger_info)
       @name = passenger_info["name"]
       @age = passenger_info["age"]
+      @driver = false
    end
 
    def adult?
       @age >= 18
+   end
+
+   def driver?
+      @driver
    end
 end
