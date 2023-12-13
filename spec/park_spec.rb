3e7528b5c65349b9b90d84a29faec38120c7d941
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-13 11:50:10 -0700

    fix&test: Create tests for attendees of Park class and fix previous test to make requirements met

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -41,13 +41,17 @@ RSpec.describe Park do
             vehicle_1.add_passenger(jude)
             vehicle_1.add_passenger(taylor)
             charlie.drive
+            vehicle_1.speed
             vehicle_2.add_passenger(liz)
             vehicle_2.add_passenger(violet)
             liz.drive
+            vehicle_2.speed
             vehicle_3.add_passenger(max)
             max.drive
             rocky_mtn.add_vehicle(vehicle_1)
             rocky_mtn.add_vehicle(vehicle_2)
+            expect(rocky_mtn.vehicles).to eq([vehicle_1 , vehicle_2])
+            rocky_mtn.add_vehicle(vehicle_3)
             expect(rocky_mtn.vehicles).to eq([vehicle_1 , vehicle_2])
         end
         it "can return a list of the passengers" do
@@ -65,11 +69,12 @@ RSpec.describe Park do
             vehicle_1.add_passenger(jude)
             vehicle_1.add_passenger(taylor)
             charlie.drive
+            vehicle_1.speed
             vehicle_2.add_passenger(liz)
             vehicle_2.add_passenger(violet)
             liz.drive
+            vehicle_2.speed
             vehicle_3.add_passenger(max)
-            max.drive
             rocky_mtn.add_vehicle(vehicle_1)
             rocky_mtn.add_vehicle(vehicle_2)
             expect(rocky_mtn.passengers).to eq([charlie, jude, taylor, liz, violet])
@@ -78,7 +83,7 @@ RSpec.describe Park do
 
     describe "#Revenue" do
         it "can return the correct revenue" do
-            rocky_mtn = Park.new("Rocky Mountain" , 100)
+            rocky_mtn = Park.new("Rocky Mountain" , 200)
             vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
             vehicle_2 = Vehicle.new("2019", "Hyundai", "SantaFe")
             vehicle_3 = Vehicle.new("2005", "Suzuki", "SX4")
@@ -92,15 +97,77 @@ RSpec.describe Park do
             vehicle_1.add_passenger(jude)
             vehicle_1.add_passenger(taylor)
             charlie.drive
+            vehicle_1.speed
             vehicle_2.add_passenger(liz)
             vehicle_2.add_passenger(violet)
             liz.drive
+            vehicle_2.speed
             vehicle_3.add_passenger(max)
             max.drive
+            vehicle_3.speed
             rocky_mtn.add_vehicle(vehicle_1)
-            expect(rocky_mtn.revenue).to eq(200)
+            expect(rocky_mtn.revenue).to eq(400)
             rocky_mtn.add_vehicle(vehicle_2)
-            expect(rocky_mtn.revenue).to eq(300)
+            expect(rocky_mtn.revenue).to eq(600)
+        end
+    end
+
+    describe "#Attendees Lists" do
+        it "Returns an alphabetically sorted list of all attendees" do
+            rocky_mtn = Park.new("Rocky Mountain" , 200)
+            vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+            vehicle_2 = Vehicle.new("2019", "Hyundai", "SantaFe")
+            vehicle_3 = Vehicle.new("2005", "Suzuki", "SX4")
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+            max = Passenger.new({"name" => "Max", "age" => 25})
+            violet = Passenger.new({"name" => "Violet", "age" => 5})
+            liz = Passenger.new({"name" => "Liz", "age" => 40})
+            vehicle_1.add_passenger(charlie)
+            vehicle_1.add_passenger(jude)
+            vehicle_1.add_passenger(taylor)
+            charlie.drive
+            vehicle_1.speed
+            vehicle_2.add_passenger(liz)
+            vehicle_2.add_passenger(violet)
+            liz.drive
+            vehicle_2.speed
+            vehicle_3.add_passenger(max)
+            max.drive
+            vehicle_3.speed
+            rocky_mtn.add_vehicle(vehicle_1)
+            rocky_mtn.add_vehicle(vehicle_2)
+            rocky_mtn.add_vehicle(vehicle_3)
+            expect(rocky_mtn.attendees).to eq(["Charlie", "Jude", "Liz", "Max", "Taylor", "Violet"])
+        end
+        it "Returns an alphabetically sorted list of all adult attendees" do
+            rocky_mtn = Park.new("Rocky Mountain" , 200)
+            vehicle_1 = Vehicle.new("2001", "Honda", "Civic")
+            vehicle_2 = Vehicle.new("2019", "Hyundai", "SantaFe")
+            vehicle_3 = Vehicle.new("2005", "Suzuki", "SX4")
+            charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+            jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+            taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+            max = Passenger.new({"name" => "Max", "age" => 25})
+            violet = Passenger.new({"name" => "Violet", "age" => 5})
+            liz = Passenger.new({"name" => "Liz", "age" => 40})
+            vehicle_1.add_passenger(charlie)
+            vehicle_1.add_passenger(jude)
+            vehicle_1.add_passenger(taylor)
+            charlie.drive
+            vehicle_1.speed
+            vehicle_2.add_passenger(liz)
+            vehicle_2.add_passenger(violet)
+            liz.drive
+            vehicle_2.speed
+            vehicle_3.add_passenger(max)
+            max.drive
+            vehicle_3.speed
+            rocky_mtn.add_vehicle(vehicle_1)
+            rocky_mtn.add_vehicle(vehicle_2)
+            rocky_mtn.add_vehicle(vehicle_3)
+            expect(rocky_mtn.adult_attendees).to eq(["Charlie", "Jude", "Liz", "Max"])
         end
     end
 end
