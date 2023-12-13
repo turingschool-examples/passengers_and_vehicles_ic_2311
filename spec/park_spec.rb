ef457e0296811b2554dc456a42d34c452155dc77
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:11:59 -0500

    Refactor: create instance variable for park in before each do block

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -3,35 +3,36 @@ require './lib/passenger'
 require './lib/vehicle'
 
 RSpec.describe do
+   before(:each) do
+      @park = Park.new("Yellowstone", 25)
+   end
+
    describe '#initialize' do
       it 'can initialize' do
-         park = Park.new("Yellowstone", 25)
-         expect(park).to be_a Park
+         expect(@park).to be_a Park
       end
 
       it 'has a name' do
-         park = Park.new("Yellowstone", 25)
-         expect(park.name).to eq "Yellowstone"
+         expect(@park.name).to eq "Yellowstone"
       end
 
       it 'has an admission price' do
-         park = Park.new("Yellowstone", 25)
-         expect(park.admission_price).to eq 25
+         expect(@park.admission_price).to eq 25
       end      
    end
 
    describe '#vehicles_entered_park' do
       it 'start as an empty array' do
-         park = Park.new("Yellowstone", 25)
-         expect(park.vehicles_entered_park).to eq []
+         expect(@park.vehicles_entered_park).to eq []
       end 
    
       it 'can list all vehicles that entered the park' do
-         park = Park.new("Yellowstone", 25)
          honda = Vehicle.new("2001", "Honda", "Civic") 
          nissan = Vehicle.new("2010", "Nissan", "Altima")
 
-         expect(park.vehicles_entered_park).to eq ([honda, nissan])
+         @park.add_vehicle(honda)
+         @park.add_vehicle(nissan)
+         expect(@park.vehicles_entered_park).to eq ([honda, nissan])
       end
    end
 end
