a6d4104d3b076bfaab3fbd2a61e2701d4b0d43dc
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-12-13 11:51:36 -0700

    feat: Create the attendees methods for Park class

--- lib/park.rb
+++ lib/park.rb
@@ -6,6 +6,7 @@ class Park
         @name = name
         @adm_price = adm_price
         @vehicles = []
+        @visitors = []
     end
 
     def admission_price
@@ -25,14 +26,41 @@ class Park
     end
     
     def add_vehicle(vehicle)
-        vehicle.speeding?
-        @vehicles << vehicle
+        (vehicle.speeding?) ? (@vehicles << vehicle) : @vehicles
     end
     def passengers
-        visitors = [] 
-        @vehicles.map do |vehicle|
-            visitors << vehicle.passengers
+        @visitors = @vehicles.map do |vehicle|
+            vehicle.passengers
+        end.flatten
+    end
+
+    def attendees_list(list)
+        @visitors = list.sort_by! do |visitor|
+            visitor.name
         end
-        visitors.flatten
+        @visitors = @visitors.map do |visitor|
+            visitor.name
+        end
+    end
+
+    def attendees
+        passengers
+        attendees_list(@visitors)
+    end
+
+    def adult_attendees
+        passengers
+        adult_attendees = @visitors.find_all do |visitor|
+            visitor.adult?
+        end
+        attendees_list(adult_attendees)
+    end
+
+    def minor_attendees
+        passengers
+        minor_attendees = @visitors.find_all do |visitor|
+            !visitor.adult?
+        end
+        attendees_list(minor_attendees)
     end
 end
