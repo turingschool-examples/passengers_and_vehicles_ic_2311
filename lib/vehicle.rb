c67a0e55cbd9d64ae4a94ee0e14e8f4f6cb2541c
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 11:41:22 -0500

    Feat: create speeding attribute and method #speeding? to access it

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,11 +1,17 @@
 class Vehicle
    attr_reader :year,
                :make,
-               :model
+               :model,
+               :speeding
    
    def initialize(year, make, model)
       @year = year
       @make = make
       @model = model
+      @speeding = false
+   end
+
+   def speeding?
+      @speeding
    end
 end
