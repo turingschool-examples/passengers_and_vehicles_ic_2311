70f088ad322012815bb5510062158700397991d0
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-13 09:07:52 -0800

    feat: Add initialize, attr reader, speeding?, speed, add passenger, num_adults methods

---
+++ lib/vehicle.rb
@@ -0,0 +1,30 @@
+class Vehicle
+    
+    attr_reader :year, :make, :model, :passengers
+
+    def initialize(year, make, model)
+        @year = year
+        @make = make
+        @model = model
+        @speeding = false
+        @passengers = []
+    end
+
+    def speeding?
+        @speeding
+    end
+
+    def speed
+        @speeding = true
+    end
+
+    def add_passenger(passenger)
+        @passengers << passenger
+    end
+
+    def num_adults
+        @passengers.count do |passenger|
+            passenger.adult?
+        end
+    end
+end
