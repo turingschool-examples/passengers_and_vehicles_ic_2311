a0e2e48137051711fc7402d815ba976ebf760285
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:06:39 -0500

    Feat: include attr_reader to access instance variables

--- lib/park.rb
+++ lib/park.rb
@@ -1,6 +1,9 @@
 class Park
-   def initialize(park_name, admission_price)
-      @park_name = park_name
+   attr_reader :name,
+               :admission_price
+   
+   def initialize(name, admission_price)
+      @name = name
       @admission_price = admission_price
    end
 end
