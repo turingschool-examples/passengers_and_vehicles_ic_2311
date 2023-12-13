b3e35f81f5c85c61a86bf628d4a12ccb84e9c3df
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 09:16:00 -0800

    feat: add_passenger to the vehicle

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -2,13 +2,15 @@ class Vehicle
   attr_reader :year,
               :make,
               :model,
-              :speeding
+              :speeding,
+              :passengers
 
   def initialize(year, make, model, speeding = false)
     @year = year
     @make = make
     @model = model
     @speeding = speeding
+    @passengers = []
   end
 
   def speeding?
@@ -17,6 +19,10 @@ class Vehicle
 
   def speed
     @speeding = true
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
   end
 
 
