bc348978176dd0474c9d4eba794f46682a7a0e26
Hoa Dam <hoangocdam@gmail.com>
2023-12-13 10:02:00 -0800

    test:calculate-revenue

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 RSpec.describe Park do
   let(:yosemite) {Park.new("Yosemite", 50)}
   let(:vehicle) {Vehicle.new("2001", "Honda", "Civic")}
+  let(:taylor) {Passenger.new({"name"=>"Taylor", "age"=>12})}
+  let(:charlie) { Passenger.new({"name"=>"Charlie", "age"=>18})}
+  let(:jude) { Passenger.new({"name"=>"Jude", "age"=>20})}
 
   describe "#initialize" do
     it "can initialize" do
       expect(yosemite).to be_a (Park)
       expect(yosemite.name).to eq ("Yosemite")
-      expect(yosemite.addmission_price).to eq (50)
+      expect(yosemite.admission_price).to eq (50)
       expect(yosemite.vehicles).to eq ([])
     end
   end
@@ -17,7 +20,30 @@ RSpec.describe Park do
     it "add a vehicle to the park" do
       yosemite.add_vehicle(vehicle)
 
+      expect(yosemite.vehicles.first).to be_a (Vehicle)
       expect(yosemite.vehicles).to eq ([vehicle])
+    end
+  end
+
+  describe "#list_of_passengers" do
+    it "return all passengers who enter the park" do
+      yosemite.add_vehicle(vehicle)
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(yosemite.list_of_passengers).to match_array ([jude, taylor, charlie])
+    end
+  end
+
+  describe "#calculate_revenue" do
+    it "calculate the revenue" do
+      yosemite.add_vehicle(vehicle)
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(yosemite.calculate_revenue).to eq (100)
     end
   end
 end
