c7a6ad636e5cda52496170b05877320b62445a09
Igor Magalhaes <magalhaess.igor@gmail.com>
2023-12-13 12:05:39 -0500

    Test: create test to access atributes of Park class

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -8,5 +8,15 @@ RSpec.describe do
          park = Park.new("Yellowstone", 25)
          expect(park).to be_a Park
       end
+
+      it 'has a name' do
+         park = Park.new("Yellowstone", 25)
+         expect(park.name).to eq "Yellowstone"
+      end
+
+      it 'has an admission price' do
+         park = Park.new("Yellowstone", 25)
+         expect(park.admission_price).to eq 25
+      end      
    end
 end
